fc5cc8f85c1f1a4bcca50838d8e9b35718260a30
Your Name <mattuti.mc@gmail.com>
2021-03-05 15:29:51 -0300

    adiciona notas e atualiza membros

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,24 @@
 package one.digitalinnovation.digionebank
 
+//outer class
 class Pessoa{
     var nome: String = "Ulysses"
     var cpf: String = "222.222.222.00"
+    //maintaining document info
     private set
+
+    //secondary constructor
+    constructor()
+
+    fun PessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val ulysses = Pessoa()
 
-    println(ulysses.nome)
-    println(ulysses.cpf)
+    println(ulysses.PessoaInfo())
+
+
 
 
 
